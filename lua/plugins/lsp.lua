d9a85f710d7dc89e5aee73804516c8855f92dd0f
r4ppz <johnreyrabosa.f@gmail.com>
2026-03-10 20:43:03 +0800

    refactor(lsp): move nvim-lsp-file-operations config to lsp module

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -62,6 +62,31 @@ return {
         automatic_enable = false,
       },
     },
+
+    {
+      "antosha417/nvim-lsp-file-operations",
+      lazy = false,
+      priority = 1000,
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-tree.lua",
+      },
+      config = function()
+        require("lsp-file-operations").setup({
+          debug = false,
+          operations = {
+            willRenameFiles = true,
+            didRenameFiles = true,
+            willCreateFiles = true,
+            didCreateFiles = true,
+            willDeleteFiles = true,
+            didDeleteFiles = true,
+          },
+          timeout_ms = 10000,
+        })
+      end,
+    },
+
     "hrsh7th/cmp-nvim-lsp",
     "nvimdev/lspsaga.nvim",
     "mfussenegger/nvim-jdtls",
