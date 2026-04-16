9388b640646a664e729f164f2a7fdc4dd39ca21a
r4ppz <johnreyrabosa.f@gmail.com>
2026-04-16 14:27:21 +0800

    feat(lua): add lazydev.nvim and simplify lua LSP configuration

--- lua/configs/servers/lua.lua
+++ lua/configs/servers/lua.lua
@@ -1,15 +1,5 @@
 local M = {}
 
-local function make_lua_library()
-  local lib = {}
-  lib[vim.fn.expand("$VIMRUNTIME/lua")] = true
-  lib[vim.fn.stdpath("data") .. "/lazy/ui/nvchad_types"] = true
-  lib[vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy"] = true
-  lib["${3rd}/luv/library"] = true
-  lib["$DOTFILES/nvim"] = true
-  return lib
-end
-
 function M.setup(capabilities)
   -- Lua LSP
   vim.lsp.config("lua_ls", {
@@ -29,9 +19,8 @@ function M.setup(capabilities)
           version = "LuaJIT",
         },
         workspace = {
-          library = make_lua_library(),
-          maxPreload = 1000,
-          preloadFileSize = 1000,
+          -- Checkout lazydev.lua
+          library = {},
           checkThirdParty = false,
         },
         telemetry = { enable = false },
---
+++ lua/plugins/lazydev.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/lazydev.nvim",
+  ft = "lua",
+  opts = {
+    library = {
+      { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      { path = "ui/nvchad_types", words = { "nvchad" } },
+      { path = "LazyVim", words = { "LazyVim" } },
+    },
+  },
+}
