dd5a8a4929cb81bfd5c36a37647378d7ac0e20a3
r4ppz <johnreyrabosa.f@gmail.com>
2026-05-27 10:09:14 +0800

    refactor(lint): simplify eslint_d args and lint trigger logic

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -4,9 +4,9 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   config = function()
     local lint = require("lint")
-
     vim.env.ESLINT_D_PPID = vim.fn.getpid()
-    lint.linters.eslint_d.args = { "--no-warn-ignored" }
+
+    table.insert(lint.linters.eslint_d.args, "--no-warn-ignored")
 
     lint.linters_by_ft = {
       javascript = { "eslint_d" },
@@ -16,13 +16,10 @@ return {
     }
 
     local lint_augroup = vim.api.nvim_create_augroup("nvim-lint", { clear = true })
-
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
       group = lint_augroup,
       callback = function()
-        if lint.linters_by_ft[vim.bo.filetype] then
-          lint.try_lint()
-        end
+        lint.try_lint()
       end,
     })
   end,
